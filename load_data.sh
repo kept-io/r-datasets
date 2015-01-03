22445f34e091447bca5b46f5edc9f396c048f362
Eduardo Arino de la Rubia <ed@ppm.io>
2015-01-03 13:16:27 -0500

    loaded data.

--- load_data.sh
+++ load_data.sh
@@ -4,5 +4,6 @@ DIR=`pwd`
 
 for object in `find csvs | grep .csv`
 do
-	mysqlimport -uroot -p$DB_PASS --delete --fields-enclosed-by='"' --fields-terminated-by=',' --ignore-lines=1 rdatasets $DIR/$object
+	echo "Executing $object" >> /tmp/import.log
+	mysqlimport -uroot -p$DB_PASS --delete --fields-enclosed-by='"' --fields-terminated-by=',' --ignore-lines=1 rdatasets $DIR/$object >> /tmp/import.log
 done
